63ae17bfe21ec5449fa898d2e4db77eb81f58cdd
lbdm44 <lewis.miller.github@pm.me>
2022-03-13 12:04:20 -0700

    setting up theme

---
+++ src/theme/SearchPage/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import styles from "./index.module.css";
+
+const SearchPage: React.FC = () => {
+  return <div className={styles.page}>SearchPage</div>;
+};
+
+export default SearchPage;
