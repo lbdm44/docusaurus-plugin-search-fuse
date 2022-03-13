13cb8ef8231a408e1cbae152c2994c8178e0c47f
lbdm44 <lewis.miller.github@pm.me>
2022-03-13 12:08:05 -0700

    setting up prettier

--- src/plugin/index.ts
+++ src/plugin/index.ts
@@ -1,12 +1,12 @@
-import type { LoadContext, Plugin } from "@docusaurus/types";
+import type { LoadContext, Plugin } from '@docusaurus/types';
 
-import type { PluginOptions } from "../../types/docusaurus-plugin-search-fuse";
+import type { PluginOptions } from '../../types/docusaurus-plugin-search-fuse';
 
 export function DocusaurusPluginSearchFuse(
   context: LoadContext,
   options: PluginOptions
 ): Plugin {
   return {
-    name: "docusaurus-plugin-search-fuse",
+    name: 'docusaurus-plugin-search-fuse',
   };
 }
--- src/theme/SearchPage/index.tsx
+++ src/theme/SearchPage/index.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React from 'react';
 
-import styles from "./index.module.css";
+import styles from './index.module.css';
 
 const SearchPage: React.FC = () => {
   return <div className={styles.page}>SearchPage</div>;
